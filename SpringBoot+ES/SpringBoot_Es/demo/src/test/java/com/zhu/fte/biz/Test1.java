c25f415521c339c491446f92547654947f75ba2c
z924931408 <924931408@qq.com>
2022-01-18 22:33:31 +0800

    kafka配置

---
+++ SpringBoot+ES/SpringBoot_Es/demo/src/test/java/com/zhu/fte/biz/Test1.java
@@ -0,0 +1,29 @@
+package com.zhu.fte.biz;
+
+
+import com.zhu.fte.biz.util.RedisUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = CacheWebApplication.class)
+@Slf4j
+public class Test1 {
+
+
+    @Resource
+    RedisUtil redisUtil;
+
+
+    @Test
+    public void getLearn(){
+        redisUtil.set("zhu","121234123");
+        log.info("打印成功");
+    }
+
+}
