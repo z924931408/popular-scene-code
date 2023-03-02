d27548979a06529668154b6635a3d54fa276b082
z924931408 <924931408@qq.com>
2023-03-02 22:59:36 +0800

    删除多余代码

--- kafka流式计算/SpringBoot-kafka-streaming/demo/src/main/java/com/zhu/fte/biz/test/singleTest/Singleton1.java
+++
@@ -1,9 +0,0 @@
-package com.zhu.fte.biz.test.singleTest;
-
-public class Singleton1 {
-    private Singleton1(){}
-    private static final Singleton1 SINGLETON_1 = new Singleton1();
-    public static Singleton1 getSingleton1(){
-        return SINGLETON_1;
-    }
-}
--- kafka流式计算/SpringBoot-kafka-streaming/demo/src/main/java/com/zhu/fte/biz/test/singleton/LazzySingleton.java
+++
@@ -1,9 +0,0 @@
-package com.zhu.fte.biz.test.singleton;
-
-public class LazzySingleton {
-    private LazzySingleton(){}
-    private static final LazzySingleton lazzy = new LazzySingleton();
-    public LazzySingleton getSingleTon(){
-        return lazzy;
-    }
-}
